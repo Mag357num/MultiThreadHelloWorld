8af7de67ef449eb78863ae54aec55e4b9bc2c92e
357Magnum <liuweining930905@gmail.com>
2022-01-13 10:38:26 +0800

    添加一些解释以及注释

--- MultiThreadHelloWorld/StdMultiThread_conditionVariable/main.cpp
+++ MultiThreadHelloWorld/StdMultiThread_conditionVariable/main.cpp
@@ -15,9 +15,10 @@ bool HasCargo()
 // 消费者线程.
 void consume(int n)
 {
-	for (int i = 0; i < n; ++i) {
-		std::unique_lock <std::mutex> lck(mtx);
-		cv.wait(lck, HasCargo);
+	for (int i = 0; i < n; ++i) 
+	{
+		std::unique_lock<std::mutex> lck(mtx);
+		cv.wait(lck, HasCargo); // HasCargo作为wait的stop_waiting条件
 		std::cout << cargo << '\n';
 		cargo = 0;
 	}
@@ -28,15 +29,18 @@ int main()
 	std::thread consumer_thread(consume, 10); // 消费者线程.
 
 	// 主线程为生产者线程, 生产 10 个物品.
-	for (int i = 0; i < 10; ++i) {
+	for (int i = 0; i < 10; ++i) 
+	{
 		while (HasCargo())
+		{
 			std::this_thread::yield();
+		}
 		std::unique_lock <std::mutex> lck(mtx);
 		cargo = i + 1;
 		cv.notify_one();
 	}
 
-	consumer_thread.join();
-
+	consumer_thread.join(); // 等待消费者线程结束
+	getchar();
 	return 0;
 }
--- MultiThreadHelloWorld/StdMultiThread_conditionVariable1/main.cpp
+++ MultiThreadHelloWorld/StdMultiThread_conditionVariable1/main.cpp
@@ -9,12 +9,13 @@ bool ready = false; // 全局标志位.
 
 void do_print_id(int id)
 {
-	std::unique_lock <std::mutex> lck(mtx);
+	std::unique_lock<std::mutex> lck(mtx); // 用同一个互斥锁来给条件变量对应的线程上锁
 	if (!ready) // 如果标志位不为 true, 则等待...
 	{
 		cv.wait(lck);
-	} // 当前线程被阻塞, 当全局标志位变为 true 之后,
-	// 线程被唤醒, 继续往下执行打印线程编号id.
+	} // 当前线程被阻塞
+	
+	// 当全局标志位变为 true 之后, 线程被唤醒, 继续往下执行打印线程编号id.
 	std::cout << "thread " << id << '\n';
 }
 
@@ -22,21 +23,26 @@ void go()
 {
 	//std::unique_lock <std::mutex> lck(mtx);
 	ready = true; // 设置全局标志位为 true.
-	cv.notify_all(); // 唤醒所有线程.
+	cv.notify_all(); // 唤醒所有线程. 所有线程之间产生竞争, 先后顺序无法确定
 }
 
 int main()
 {
-	std::thread threads[10];
-	// spawn 10 threads:
-	for (int i = 0; i < 10; ++i)
-		threads[i] = std::thread(do_print_id, i);
+	std::thread threads[10]; // spawn 10 threads
 
+	for (int i = 0; i < 10; ++i)
+	{
+		threads[i] = std::thread( do_print_id, i ); // 使用函数do_print_id和其参数i初始化线程
+	}
 	std::cout << "10 threads ready to race...\n";
+
 	go(); // go!
 
 	for (auto& th : threads)
-		th.join();
+	{
+		th.join(); // 阻塞, 等待所有线程结束
+	}
+
 	getchar();
 	return 0;
 }
